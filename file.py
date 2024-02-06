162c19cc9b6ab2dfcb5a2317e36c7132f097c02d
Joqsan Azocar <joqsan.a@gmail.com>
2024-02-06 21:54:50 +0300

    add minus code to file.py

---
+++ file.py
@@ -0,0 +1,11 @@
+def subtract_numbers(a, b):
+    return a - b
+
+class MyClass:
+    def __init__(self) -> None:
+        self.a = 5555
+        self.b = 8888
+    
+    def minus_numbers(self):
+        return self.a - self.b
+
