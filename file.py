d1b2f6014f0c92bfcc0b225184dae860bcc24767
Joqsan Azocar <joqsan.a@gmail.com>
2024-02-06 21:53:37 +0300

    add code to file.py

---
+++ file.py
@@ -0,0 +1,10 @@
+def sum_numbers(a, b):
+    return a + b
+
+class MyClass:
+    def __init__(self) -> None:
+        self.a = 5
+        self.b = 8
+
+    def add_numbers(self):
+        return self.a + self.b
